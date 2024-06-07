93fc3081bce8702a4b46fa662a476574a3962db5
Onelio Viera <ojvwebdesign@gmail.com>
2024-06-07 14:14:36 -0600

    refactor: Update Navbar links styling for better readability and accessibility

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,12 +9,12 @@ const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="px-4 py-6 bg-gray-400 shadow-lg border-b-2 border-b-gray-400">
+    <nav className="px-4 py-6 bg-gray-400 shadow-lg border-b-2 border-b-red-600">
 
       <div className='mx-auto max-w-7xl sm:px-6 lg:px-6'>
         <div className='relative flex items-center justify-between'>
           <Link href='/'>
-            <h1 className="hidden lg:flex md:flex text-2xl text-white lg:text-left md:text-left">Navbar</h1>
+            <h1 className="hidden lg:flex md:flex text-2xl text-red-600 lg:text-left md:text-left">Navbar</h1>
             </Link>
           <div className='flex gap-4 text-lg'>
             <Link href='/' className={`${pathname === '/' ? 'font-bold border-b-2 border-red-600' : ''} text-white hover:font-bold hover:text-red-600 px-3 py-2`}>
